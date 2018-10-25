50de914bd0c80d9feca4913daea4de1399266b68
melwyn2021 <34481768+melwyn2021@users.noreply.github.com>
2018-10-24 20:40:04 -0400

    Update Mecanum.java
    fixed turn issue

--- src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -3,10 +3,10 @@
  */
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DcMotor;
 public class Mecanum {
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
@@ -17,15 +17,19 @@ public class Mecanum {
         frontLeft = h.get(DcMotor.class, "frontLeft");
         frontRight = h.get(DcMotor.class, "frontRight");
         backLeft = h.get(DcMotor.class, "backLeft");
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
     }
     public void move(Gamepad gamepad){
         double theta = Math.atan2(gamepad.left_stick_x, gamepad.left_stick_y)+(Math.PI/4);
-        double r = Math.hypot(gamepad.left_stick_x,gamepad.left_stick_y);
-        double turn = Math.hypot(gamepad.right_stick_x,gamepad.right_stick_y);
-        final double fr = Range.clip(((Math.cos(theta) * r ) + turn),-1,1);
-        final double fl = Range.clip(((Math.cos(theta) * -r ) + turn),-1,1);
-        final double br = Range.clip(((Math.sin(theta) * -r ) + turn),-1,1);
-        final double bl = Range.clip(((Math.sin(theta) * r ) + turn),-1,1);
+        double r = Range.clip(Math.hypot(gamepad.left_stick_x,gamepad.left_stick_y),-1,1);
+        double turn = -gamepad.right_stick_x;
+        double fr = Math.cos(theta) * r + turn;
+        double fl = Math.sin(theta) * r - turn;
+        double br = Math.sin(theta) * r + turn;
+        double bl = Math.cos(theta) * r - turn;
         backLeft.setPower(bl);
         backRight.setPower(br);
         frontLeft.setPower(fl);
