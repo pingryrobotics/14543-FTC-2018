701cd04fb38e2fa8e2fc04dbc702ff2b8c4c2a1e
cgu2022 <cgu2022@pingry.org>
2018-11-09 16:51:26 -0500

    Changed set Elbow

--- src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -10,7 +10,7 @@ public class Arm {
     private CRServo collection1 = null;
     private CRServo collection2 = null;
     private Servo elbow1;
-    private Servo elbow2;
+    private Servo elbow2; //right
     private Servo wrist;
     public Arm(HardwareMap h){
         shoulder = h.get(DcMotor.class,"shoulder");
@@ -19,18 +19,20 @@ public class Arm {
         collection2.setDirection(DcMotorSimple.Direction.REVERSE);
         elbow1 = h.get(Servo.class, "elbow1");
         elbow2 = h.get(Servo.class, "elbow2");
-        elbow2.setDirection(Servo.Direction.REVERSE);
+        elbow2.setDirection(Servo.Direction.REVERSE); //right; 0 is now the 180
+        elbow1 = scaleRange(0.0, 0.66); //Set position is still 0-1, but the 1 = 180 not 1 = 270
+        elbow2 = scaleRange(0.0, 0.66);
         wrist = h.get(Servo.class, "wirst");
-
+        elbow1.setPosition(0.5);
+        elbow2.setPosition(0.5);
     }
     public void setShoulder(double targetPos){
         shoulder.setPower(targetPos);
     }
-    public void setElbow(double targetPos) {
-        targetPos += 1;
-        targetPos /= 2;
-        elbow1.setPosition(targetPos);
-        elbow2.setPosition(targetPos);
+    public void setElbow(double diffPos) { //Precondition: diffPos is a decimal to increment/decrement
+        double current = elbow1.getPosition();
+        elbow1.setPosition(current + diffPos);
+        elbow2.setPosition(targetPos + diffPos);
     }
     public void intake(){
         collection1.setPower(-1);
